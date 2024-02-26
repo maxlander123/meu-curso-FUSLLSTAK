93dd47ddbca2235667fdfb85cc26292375cad75d
max <maxlander2019@gmail.com>
2024-02-26 16:53:17 -0300

    TODO DIA

--- Módulo 02/07 - Interação com API/aula 06/exemplo 02/exemplo-01.js
+++ Módulo 02/07 - Interação com API/aula 06/exemplo 02/exemplo-01.js
@@ -2,4 +2,18 @@ async function concessionariaa(){
     let resultado = await fetch("moto.json")
     let moto = await resultado.json()
 
+    let grupodiv = document.getElementById("lista-card")
+
+    for(let produto of produtos){
+        grupodiv.innerHTML +=`
+        <div class = "card">
+        <h3>${produto.nome}</h3>
+        <p>${produto.marca}</p>
+
+        </div>
+        `
+        
+
+    }
+
 }
