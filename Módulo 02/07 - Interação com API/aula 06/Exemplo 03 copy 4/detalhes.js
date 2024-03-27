57dcaa6882f11127fc678886bbb79bf5b43efdd1
max <maxlander2019@gmail.com>
2024-03-27 17:23:17 -0300

    acabou

---
+++ Módulo 02/07 - Interação com API/aula 06/Exemplo 03 copy 4/detalhes.js
@@ -0,0 +1,41 @@
+async function buscarDetalhes(){
+    let busca = await fetch("lista-produtos.json")
+    let produtos = await busca.json()
+    console.log(produtos)
+   // document.body.
+   //criou um objeto URLSarchparams e passou
+   //coleta dos parametros da URL nele.
+   let paramentros = new URLSearchParams(window.location.search)
+   // obteve do pararetro "produto-id ", o seu valor 
+   let paramentroId = paramentros.get("produto-id")
+
+   let indicePROd
+   for(let x in produtos){
+        if(produtos[x].id == paramentroId){
+            indicePROd = x
+        }
+   }
+
+   document.getElementById(Detalhes).innerHTML =`
+        <h3>${produtos[indicePROd].nome}</h3>
+        <img src="${produtos[indicePROd].img[0]}" width="250" height="250">
+
+
+
+
+
+        
+           `
+           let divMiniaturas = Document.getElementById("miniatura")
+            for(let y of produtos [indicePROd].img){
+                divMiniaturas.innerHTML +=`
+               <img src="${y} " class="mini" width="80" height="80" style="
+                `
+            }
+
+   
+   //alert(paramentroId)
+
+}
+
+buscarDetalhes()
---
+++ Módulo 02/07 - Interação com API/aula 06/Exemplo 03 copy 4/index.js
@@ -0,0 +1,41 @@
+    // FUNÇAO ASSINCRONA PARA FAZER USO DO  AWAIT
+        async function busca(){
+        //await PARA a exercuçao do programa aquardando
+        // O RESULTADO DO COMANDO FETCH().
+        let procura= await fetch("lista-produtos.json")
+        let produtos= await procura.json()
+
+        let divlista = document.getElementById("lista-card")
+        for(let produto of produtos){
+            divlista.innerHTML +=`
+                <div class="card"> data-id="${produto.id}">
+                    <h3>${produto.nome}</h3>
+                    <img src="${produto.img[0]}" width="200" height="200">
+                     <p>${produto.descricao}</p>
+                    <div class="valores">
+                        <span class="valorcom">R$ ${produto.valorcomdesconto.toFixed(2).replace(".",",")}</span>
+                        <span class="valorsem">R$ ${produto.valorsemdesconto.toFixed(2).replace(".",",")}</span>
+                    <div>
+                </div>    
+            `
+        
+        }
+        //busca por todos elementos html que contem"card"
+        // como valor do paramentro "class".
+        let divsCards = document.getElementsByClassName("card")
+        // Add em cada Div card um evento que escuta quando
+        // o usuario clica nele ,e chama uma funçao.
+        for(let card of divsCards){
+            card.addEventListener("click",clicou)
+
+
+        }
+
+}
+    function clicou(){
+        console.log(this)
+        let elementoId = this.getAttribute("data-id")
+        window.location.href = "detalhes.html?produto-id=" + elementoId
+        // window.location.href = "detalhes.html?"produtos-id="+ e"
+    } 
+busca()                       
